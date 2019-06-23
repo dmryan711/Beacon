ecc84a7f941e233e57d6d6b7eac80af1d5164fb1
Devon <dmryan711@users.noreply.github.com>
2019-06-23 16:41:57 -0400

    Cluster 2 (#7)
    * Holy shit its working
    
    * Cluster working with beacons
    
    * Cleaned up files, cluster is working
    
    * Cleaned up files, cluster is working

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,26 +3,31 @@ import React from "react";
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import BeaconMarker from "./BeaconMarker";
 
+const { MarkerClusterer } = require("react-google-maps/lib/components/addons/MarkerClusterer");
+
+
 const Map = withScriptjs(withGoogleMap((props) =>{
-
-  const markers = props.beacons.map( (beacon,index) => 
-                  <BeaconMarker
-                    location={{lat: beacon.location.lat, lng: beacon.location.lon}}
-                    key = {index}
-  />);
-
-  
-
-
-
-                  
+                
   return (
       <GoogleMap
         defaultZoom={14}
         center={ { lat:  40.8089897, lng: -73.9612492 } }
         onClick={props.mapClick}
         >
-        {markers}
+        <MarkerClusterer
+      onClick={props.onMarkerClustererClick}
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.beacons.map((beacon,index) => (
+        <BeaconMarker
+        location={{lat: beacon.location.lat, lng: beacon.location.lon}}
+        key = {index}
+        />
+      ))}
+    </MarkerClusterer>
+        
       </GoogleMap>
     );
   }
