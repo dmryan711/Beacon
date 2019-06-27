6e836fd13ceff9200252459df9b082b1340afe53
Devon <dmryan711@gmail.com>
2019-06-27 13:11:18 -0400

    Added icons and beacon marker logic to component

--- client/src/components/MapControl.js
+++ client/src/components/MapControl.js
@@ -1,8 +1,6 @@
 //MapControl.js
 import React from "react";
-import { Marker } from "react-google-maps";
-import partyIcon from "../assets/images/beaconParty.png";
-import userIcon from "../assets/images/currentLocation.png"
+
 
 function MapControl(props){
     return(
