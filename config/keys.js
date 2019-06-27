18697ff34c3bed829f9bcf311192553d59aa5e15
Rahul Vig <rahvee51395@gmail.com>
2019-06-27 10:04:36 -0400

    user authentication

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://beacon:beacon@beacon-puenn.gcp.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -1,43 +1,63 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const passport = require("passport");
+const users = require("./routes/api/users");
 const app = express();
 
-const db = require("./models");
-
 // Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// If deployed, use the deployed database. Otherwise use the local Beacon database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Beacon";
+const db = require("./config/keys").mongoURI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local Beacon database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Beacon";
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 // Define API routes here
 
-app.post("/api/test",(req,res) =>{
-  const testName = req.body.testName;
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
-  db.Test.create({
-    testName: testName
-  }).then(function(test){
-    console.log(test._id);
-    res.sendStatus(200);
-    console.log("[DEBUG]  WORKING");
+// app.post("/api/test", (req, res) => {
+//   const testName = req.body.testName;
 
-  }).catch(function(err){
-    console.log(err.message);
-    console.log("[DEBUG] NOT WORKING");
+//   db.Test.create({
+//     testName: testName
+//   })
+//     .then(function(test) {
+//       console.log(test._id);
+//       res.sendStatus(200);
+//       console.log("[DEBUG]  WORKING");
+//     })
+//     .catch(function(err) {
+//       console.log(err.message);
+//       console.log("[DEBUG] NOT WORKING");
 
-    res.sendStatus(400);
-  });
-
-});
+//       res.sendStatus(400);
+//     });
+// });
 
 // Send every other request to the React app
 // Define any API routes before this runs
